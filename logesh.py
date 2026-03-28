ceeb6d7a522973e72b3b3b0c333ab2c022866180
Logeshwaran V <thanks369963@gmail.com>
2026-03-28 16:49:44 +0530

    Initial commit: added palindrome script

---
+++ logesh.py
@@ -0,0 +1,16 @@
+import sys
+
+
+if len(sys.argv) < 2:
+    print("Error: No word provided.")
+    sys.exit(1)
+
+word = sys.argv[1].lower().replace(" ", "")
+
+
+if word == word[::-1]:
+    print(f"Success: '{word}' is a palindrome!")
+    sys.exit(0)
+else:
+    print(f"Fail: '{word}' is not a palindrome.")
+    sys.exit(1)
