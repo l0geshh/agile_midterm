bcafb4ce23de85acc88c851b8c5da1565e31e2e7
Logeshwaran V <thanks369963@gmail.com>
2026-03-28 21:43:02 +0530

    Triggering auto build

--- logesh.py
+++ logesh.py
@@ -14,4 +14,4 @@ if word == word[::-1]:
 else:
     print(f"Fail: '{word}' is not a palindrome.")
     sys.exit(1)
-    # Final Midterm Test for Lab TechkaksksdafwsjjdsjASDASDAASDAS
+    # Final Midterm Test for Lab TechkaksksdafwsjjdsjASDASDAASDASASDASA
